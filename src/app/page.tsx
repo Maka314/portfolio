713fb19ce39a53e268646ff3b11f7885590b64d8
mingchen <traveller31415@gmail.com>
2024-12-04 09:26:40 +0000

    新增Skills和Footer组件，更新主页以包含新组件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Hero from '@/components/Hero'
-import Projects from '@/components/Projects'
+import Hero from '@/components/Hero';
+import Projects from '@/components/Projects';
+import Skills from '@/components/Skills';
+import Footer from '@/components/Footer';
 
 export default function Home () {
   return (
     <main className='relative flex flex-col justify-center items-center overflow-hidden mx-auto'>
       <Hero />
       <Projects />
+      <Skills />
+      <Footer />
     </main>
   )
 }
---
+++ src/components/Skills.tsx
@@ -0,0 +1,7 @@
+export default function Skills() {
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <h1>Skills</h1>
+    </div>
+  )
+}
