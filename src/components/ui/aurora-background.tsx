ddc06c1ca7ac1ff900a5813a632003118122653b
mingchen <traveller31415@gmail.com>
2024-12-04 09:10:46 +0000

    重构主页和背景组件，优化样式模块，删除旧的样式文件并引入新的模块化样式

--- src/components/ui/aurora-background.tsx
+++ src/components/ui/aurora-background.tsx
@@ -1,5 +1,5 @@
-'use client';
-import style from './style.module.css';
+'use client'
+import style from './aurora-background.module.css'
 import React, { ReactNode } from 'react'
 
 interface AuroraBackgroundProps extends React.HTMLProps<HTMLDivElement> {
@@ -11,20 +11,16 @@ export const AuroraBackground = ({
   ...props
 }: AuroraBackgroundProps) => {
   return (
-    <main>
-      <div
-        className={
-          'relative flex flex-col  h-[100vh] items-center justify-center bg-zinc-50 dark:bg-zinc-900  text-slate-950 transition-bg'
-        }
-        {...props}
-      >
-        <div className='absolute inset-0 overflow-hidden'>
-          <div
-            className={style.auroraBack}
-          ></div>
-        </div>
-        {children}
+    <div
+      className={
+        'relative flex flex-col  h-[100vh] items-center justify-center bg-zinc-50 dark:bg-zinc-900  text-slate-950 transition-bg w-full'
+      }
+      {...props}
+    >
+      <div className='absolute inset-0 overflow-hidden'>
+        <div className={style.auroraBack}></div>
       </div>
-    </main>
+      {children}
+    </div>
   )
 }
