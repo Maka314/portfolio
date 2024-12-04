6f678f9eac090b14febd191f67611113bec4fbbe
mingchen <traveller31415@gmail.com>
2024-12-04 06:07:00 +0000

    删除测试页面的样式模块和相关组件，优化样式文件

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import style from './page.module.css';
-
-export default function Home() {
-    return (
-        <div className={style.box}>
-            Hello world! <br />another line
-        </div>
-    );
-}
