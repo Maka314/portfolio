a1834f0972445c343f527bf780a0ee21addfa389
mingchen <traveller31415@gmail.com>
2024-12-04 09:20:56 +0000

    重构主页组件，移除旧的AuroraBackground，新增Hero和Projects组件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import { AuroraBackground } from '@/components/ui/AuroraBackground'
+import Hero from '@/components/Hero'
+import Projects from '@/components/Projects'
 
 export default function Home () {
   return (
     <main className='relative flex flex-col justify-center items-center overflow-hidden mx-auto'>
-        <AuroraBackground>
-          <h1 className='hero_heading'>
-            Welcome to my new portfolio!
-            <br />
-            This page is still under construction.
-          </h1>
-        </AuroraBackground>
+      <Hero />
+      <Projects />
     </main>
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import { AuroraBackground } from '@/components/ui/AuroraBackground'
+
+export default function Hero () {
+  return (
+    <AuroraBackground>
+      <h1 className='hero_heading'>
+        Welcome to my new portfolio!
+        <br />
+        This page is still under construction.
+      </h1>
+    </AuroraBackground>
+  )
+}
