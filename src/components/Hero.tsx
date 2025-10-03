49adbd24d6347e28aa45c2476dfc0559d6c4f991
Mingchen Ju <traveller31415@gmail.com>
2025-10-03 11:01:04 +0000

    添加新组件，title 轮播机制

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,17 @@
 'use client';
 
 import { motion } from 'motion/react';
+import RotatingText from '@/components/ui/rotating_text/RotatingText';
 
 import { AuroraBackground } from '@/components/ui/aurora_background/AuroraBackground';
+
+const title_list = [
+  'Software Engineer',
+  'Web Developer',
+  'Project Manager',
+  'Tech Enthusiast',
+  'Learner',
+];
 
 export default function Hero() {
   return (
@@ -20,8 +29,20 @@ export default function Hero() {
         id='hero'
       >
         <h1 className='hero_heading'>
-          Hello, I&apos;m <span className='font-medium'>Michael Ju</span> <br />{' '}
-          A full stack developer
+          Hi! I&apos;m <span className='font-medium'>Mingchen Ju</span> <br />{' '}
+          A
+          <RotatingText
+            texts={title_list}
+            mainClassName="px-2 sm:px-2 md:px-3 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+            staggerFrom={"last"}
+            initial={{ y: "100%" }}
+            animate={{ y: 0 }}
+            exit={{ y: "-120%" }}
+            staggerDuration={0.025}
+            splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+            transition={{ type: "spring", damping: 30, stiffness: 400 }}
+            rotationInterval={2000}
+          />
         </h1>
       </motion.section>
     </AuroraBackground>
