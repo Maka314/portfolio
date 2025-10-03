46f7ad2be4a926656fca3ab3e75f74b3461bab9a
Mingchen Ju <traveller31415@gmail.com>
2025-10-03 11:14:17 +0000

    更新数据格式

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,30 +1,48 @@
 import SectionHeading from '@/components/ui/section_heading/SectionHeading';
 import ProjectCard from '@/components/ui/project_card/ProjectCard';
 
-import fs from 'fs';
-import path from 'path';
-
-function getProjects() {
-  const filePath = path.join(process.cwd(), 'config', 'projects.json');
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-  const projects = JSON.parse(fileContents);
-
-  return projects;
+// 项目数据类型定义
+interface ProjectData {
+  projectName: string;
+  projectImage: string;
+  description: string;
+  projectLink: string;
 }
 
+// 项目数据直接定义在组件中
+const projectsData: ProjectData[] = [
+  {
+    projectName: "FX Risk Control System",
+    projectImage: "/images/project2_cover.png",
+    description: "Monitoring of foreign exchange trading behavior using machine learning methods",
+    projectLink: "https://www.traveller314.com"
+  },
+  {
+    projectName: "LLM Industry Application",
+    projectImage: "/images/project2_cover.png",
+    description: "LLM deployed in actual production environments is used to simplify customer service processes and intelligent services on the business side.",
+    projectLink: "https://www.traveller314.com"
+  },
+  {
+    projectName: "First Place in Neara Hackathon",
+    projectImage: "/images/project3_cover.png",
+    description: "First place in the hackathon competition held by UNSW in collaboration with Neara, out of more than 30 teams. Development language is kotlin",
+    projectLink: "https://www.traveller314.com"
+  }
+];
+
 export default function Projects() {
-  const projectsData = getProjects();
   return (
     <div className='flex flex-col justify-center items-center h-auto py-12'>
       <SectionHeading>Projects</SectionHeading>
       <div className='flex flex-row justify-center items-start flex-wrap'>
-        {Object.keys(projectsData).map((key) => (
-          <ProjectCard 
-            key={key}
-            projectName={projectsData[key].projectName}
-            projectDescription={projectsData[key].description}
-            projectLink={projectsData[key].projectLink}
-            projectImage={projectsData[key].projectImage}
+        {projectsData.map((project, index) => (
+          <ProjectCard
+            key={index}
+            projectName={project.projectName}
+            projectDescription={project.description}
+            projectLink={project.projectLink}
+            projectImage={project.projectImage}
           />
         ))}
       </div>
