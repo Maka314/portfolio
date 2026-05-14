44a743bd1d500309802ba2e9f814eb4ff3132704
Mingchen Ju <traveller31415@gmail.com>
2026-05-14 08:48:54 +0000

    refactor: remove unused dependencies and components
    - Deleted `clsx`, `gray-matter`, `react-markdown`, and `tailwind-merge` from dependencies in `package.json`.
    - Removed `Skills` component from `src/components/Skills.tsx`.
    - Eliminated markdown utility functions from `src/lib/markdown.ts`.

--- src/components/Skills.tsx
+++
@@ -1,9 +0,0 @@
-import SectionHeading from '@/components/ui/section_heading/SectionHeading';
-
-export default function Skills() {
-  return (
-    <div className='flex flex-col justify-center items-center'>
-      <SectionHeading>Skills</SectionHeading>
-    </div>
-  )
-}
