ad7bc1243e7f92cb07c3bbea3cb886a23e0c2520
Mingchen Ju <traveller31415@gmail.com>
2026-05-14 06:46:14 +0000

    修正项目链接路径，确保与路由一致

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,25 +15,25 @@ const projectsData: ProjectData[] = [
   //   projectName: "AI Regulatory Assistant",
   //   projectImage: "/images/project2_cover.png",
   //   description: "A large language model–based regulatory assistant for the construction field that helps users quickly search and understand relevant regulations, improving work efficiency.",
-  //   projectLink: "/posts/ai-regulation-assistant"
+  //   projectLink: "/post/ai-regulation-assistant"
   // },
   {
     projectName: "FX Risk Control System",
     projectImage: "/images/project2_cover.png",
     description: "Monitoring of foreign exchange trading behavior using machine learning methods",
-    projectLink: "/posts/fx-risk-control-system"
+    projectLink: "/post/fx-risk-control-system"
   },
   {
     projectName: "LLM Industry Application",
     projectImage: "/images/project2_cover.png",
     description: "LLM deployed in actual production environments is used to simplify customer service processes and intelligent services on the business side.",
-    projectLink: "/posts/llm-industry-application"
+    projectLink: "/post/llm-industry-application"
   },
   {
     projectName: "Neara Hackathon Winner",
     projectImage: "/images/project3_cover.png",
     description: "First place in the hackathon competition held by UNSW in collaboration with Neara, out of more than 30 teams. Development language is kotlin",
-    projectLink: "/posts/first-place-in-neara-hackathon"
+    projectLink: "/post/first-place-in-neara-hackathon"
   }
 ];
 
